db3f122e3ceca44afa43967d8bdb092e3cf103c3
Alex <alex.nekrashevich@akvelon.com>
2021-09-14 17:31:42 +0300

    Lint

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
 async function getBranches() {
   const branchData = await fetch(`https://api.appcenter.ms/v0.1/apps/${process.env.USER}/${process.env.APP}/branches`, {
     headers: { 'X-API-TOKEN': process.env.TOKEN }
-  })
-  return branchData.json()
-}
+  });
+  return branchData.json();
+};
 
 async function startBuild(branchName) {
   console.log('Build in queue: ' + branchName)
@@ -15,26 +15,26 @@ async function startBuild(branchName) {
     method: 'POST',
     headers: { 'X-API-TOKEN': process.env.TOKEN }
   })
-  .catch(err => console.log(err))
-}
+  .catch(err => console.log(err));
+};
 
 async function createSuccessMessage(buildData, branchName, result) {
-    const startTime = new Date(buildData.startTime).getTime()
-    const finishTime = new Date(buildData.finishTime).getTime()
-    const buildTime = (finishTime - startTime) / 1000
+    const startTime = new Date(buildData.startTime).getTime();
+    const finishTime = new Date(buildData.finishTime).getTime();
+    const buildTime = (finishTime - startTime) / 1000;
     await fetch (`https://api.appcenter.ms/v0.1/apps/${process.env.USER}/${process.env.APP}/builds/${buildData.id}/downloads/logs`, {
       headers: { 'X-API-TOKEN': process.env.TOKEN }
     })
     .then(res => res.json())
     .then(logs => {
       if (result === 'succeeded') {
-        console.log('Build succeeded: ' + branchName + '\n Logs: ' + logs.uri + '\n Build time: ' + buildTime + ' seconds')
+        console.log('Build succeeded: ' + branchName + '\n Logs: ' + logs.uri + '\n Build time: ' + buildTime + ' seconds');
       } else {
-        console.log('Build failed: ' + branchName + '\n Logs: ' + logs.uri)
+        console.log('Build failed: ' + branchName + '\n Logs: ' + logs.uri);
       }
     })
-    .catch(err => console.log(err))
-}
+    .catch(err => console.log(err));
+};
 
 async function checkBuild(branchName, timer) {
   await fetch(`https://api.appcenter.ms/v0.1/apps/${process.env.USER}/${process.env.APP}/branches/${branchName}/builds`, {
@@ -44,26 +44,26 @@ async function checkBuild(branchName, timer) {
   .then(builds => builds[0])
   .then(async buildData => {
     if (buildData.status === 'completed') {
-      await createSuccessMessage(buildData, branchName, buildData.result)
-      clearInterval(timer)
+      await createSuccessMessage(buildData, branchName, buildData.result);
+      clearInterval(timer);
       }
     }
   )
-  .catch(err => console.log(err))
-
-}
+  .catch(err => console.log(err));
+};
 
 async function buildAllBranches() {
   for (let branchObject of await getBranches()) {
-    await startBuild(branchObject.branch.name)
-    const interval = 10000
-    const timer = setInterval(async() => await checkBuild(branchObject.branch.name, timer), interval)
+    await startBuild(branchObject.branch.name);
+    const interval = 10000;
+    const timer = setInterval(async() => await checkBuild(branchObject.branch.name, timer), interval);
   }
-}
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await buildAllBranches()
+  await buildAllBranches();
   await app.listen(3000);
-}
+};
 
 bootstrap();
